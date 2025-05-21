6e366dbbe7546cf6957873f0dd77eb433602cfe0
diaztradeinc <79354866+diaztradeinc@users.noreply.github.com>
2025-05-20 21:32:54 -0400

    Delete assets directory

--- assets/js/sortable.js
+++
@@ -1,8 +0,0 @@
-
-// Lightweight Sortable mock (replace with full Sortable.js if needed)
-window.Sortable = {
-  create: function (el, opts) {
-    console.log("Sortable enabled on:", el);
-    // Mock drag behavior (to be replaced with full lib in real use)
-  }
-};
